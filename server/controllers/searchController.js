09c05d3376892adf94d7c96cdae9ca0e6b17f40d
Than <than.jarvis@gmail.com>
2019-10-14 16:44:34 -0600

    finished project

---
+++ server/controllers/searchController.js
@@ -0,0 +1,15 @@
+const swag = require('../models/swag')
+
+module.exports = {
+    search: (req, res) => {
+        const {category} = req.query
+        if(!category){
+            res.status(200).send(swag)
+        }else{
+            const filteredSwag = swag.filter(swag => swag.category === category)
+            res.status(200).send(filteredSwag)
+        }
+        console.log(category);
+        
+    }
+}
---
+++ server/middlewares/checkForSession.js
@@ -0,0 +1,12 @@
+module.exports = function(req, res, next){
+    const {session} = req
+    if(!req.session.user){
+        session.user = {
+            username: '',
+            cart: [],
+            total: 0
+        }
+    }
+    next()
+}
+
